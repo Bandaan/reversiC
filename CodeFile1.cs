1987d55eb387e65c3cb339e3c7da277d9c5ed417
Bandaan <rensdaan2002@gmail.com>
2022-12-13 12:16:57 +0100

    Main file gemaakt - Daan

---
+++ CodeFile1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ReversiC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form scherm = new Form();
+            scherm.Text = "ReversiC";
+            scherm.BackColor = Color.LightYellow;
+            scherm.ClientSize = new Size(500, 500);
+
+
+            Application.Run(scherm);
+        }
+
+    }
+
+}
